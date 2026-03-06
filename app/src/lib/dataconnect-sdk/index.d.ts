2ea4f830d9e5ef1ba6e963c18dec52126f7c2119
Kaden-Beck <kadenb12@gmail.com>
2026-03-06 14:25:51 -0700

    Refactor code structure for improved readability and maintainability

--- app/src/lib/dataconnect-sdk/index.d.ts
+++ app/src/lib/dataconnect-sdk/index.d.ts
@@ -1,12 +1,50 @@
-import { ConnectorConfig, DataConnect } from 'firebase/data-connect';
+import { ConnectorConfig } from 'firebase/data-connect';
+
 export const connectorConfig: ConnectorConfig;
 
 export type TimestampString = string;
-
 export type UUIDString = string;
-
 export type Int64String = string;
-
 export type DateString = string;
 
+
+
+
+export interface Actor_Key {
+  id: UUIDString;
+  __typename?: 'Actor_Key';
+}
+
+export interface FavoriteMovie_Key {
+  userId: string;
+  movieId: UUIDString;
+  __typename?: 'FavoriteMovie_Key';
+}
+
+export interface MovieActor_Key {
+  movieId: UUIDString;
+  actorId: UUIDString;
+  __typename?: 'MovieActor_Key';
+}
+
+export interface MovieMetadata_Key {
+  id: UUIDString;
+  __typename?: 'MovieMetadata_Key';
+}
+
+export interface Movie_Key {
+  id: UUIDString;
+  __typename?: 'Movie_Key';
+}
+
+export interface Review_Key {
+  userId: string;
+  movieId: UUIDString;
+  __typename?: 'Review_Key';
+}
+
+export interface User_Key {
+  id: string;
+  __typename?: 'User_Key';
+}
 
